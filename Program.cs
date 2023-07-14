3ad08136c8fcc79d8b4a35e49469c09cdc556a74
jeeban <jeebanjyoti193@gmail.com>
2023-07-14 16:05:04 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+
+namespace Day5_Assignment_07
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList fruits = new ArrayList();
+            fruits.Add("apple");
+            fruits.Add("banana");
+            fruits.Add("cherry");
+            fruits.Add("date");
+            fruits.Add("elderberry");
+
+
+            Console.WriteLine("Total number of elements: " + fruits.Count);
+            Console.WriteLine("Total fruits in the list: ");
+            for (int i = 0; i < fruits.Count; i++)
+            {
+                Console.WriteLine(fruits[i]);
+            }
+
+
+            Console.Write("Enter an element to search: ");
+            string searchElement = Console.ReadLine();
+            bool containsSearchElement = fruits.Contains(searchElement);
+            Console.WriteLine($"Contains '{searchElement}': {containsSearchElement}");
+
+            Console.Write("Enter an element to insert at the second position: ");
+            string insertElement = Console.ReadLine();
+            fruits.Insert(1, insertElement);
+
+            Console.Write("Enter an element to remove: ");
+            string removeElement = Console.ReadLine();
+            fruits.Remove(removeElement);
+
+            Console.WriteLine("Elements in the ArrayList:");
+            foreach (var fruit in fruits)
+            {
+                Console.WriteLine(fruit);
+            }
+        }
+    }
+}
